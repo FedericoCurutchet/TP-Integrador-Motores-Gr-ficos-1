1cedce118ab62549dd9e15646be379b3a7020d0b
FedericoCurutchet <114716003+FedericoCurutchet@users.noreply.github.com>
2022-11-22 22:34:38 -0300

    Ganar y Perder
    Se agrego la funcion de ganar y perder dentro del juego.

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -1,5 +1,6 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ControlJugador : MonoBehaviour
@@ -14,6 +15,11 @@ public class ControlJugador : MonoBehaviour
     public GameObject Linterna;
     public GameObject Linterna2;
 
+    public GameObject Mapa;
+    public GameObject Combustible;
+    public GameObject Tarjeta;
+    public GameObject LlaveAuto;
+
     public Light LuzLinterna;
 
     public bool luzactivada = false;
@@ -24,6 +30,11 @@ public class ControlJugador : MonoBehaviour
     public TMPro.TMP_Text textoVida;
     public TMPro.TMP_Text textoBotiquin;
     public TMPro.TMP_Text textoObjetos;
+    public TMPro.TMP_Text textoMapa;
+    public TMPro.TMP_Text textoLlave;
+    public TMPro.TMP_Text textoTarjeta;
+    public TMPro.TMP_Text textoComb;
+    public TMPro.TMP_Text textoHUD;
 
     public int municion = 10;
     public int municionesc = 6;
@@ -39,6 +50,11 @@ public class ControlJugador : MonoBehaviour
         textoVida.text = "";
         textoBotiquin.text = "";
         textoObjetos.text = "";
+        textoMapa.text = "";
+        textoLlave.text = "";
+        textoTarjeta.text = " ";
+        textoComb.text = "";
+        textoHUD.text = "";
         Cursor.lockState = CursorLockMode.Locked;
         mostrarTextos();
     }
@@ -56,9 +72,14 @@ public class ControlJugador : MonoBehaviour
         textoVida.text = "VIDA: " + hp.ToString();
         textoBotiquin.text = "Botiquines: " + botiquin.ToString();
         textoObjetos.text = "Objetos Restantes: " + obj.ToString();
+        textoMapa.text = "Mapa";
+        textoLlave.text = "Llave del Auto";
+        textoTarjeta.text = "Tarjeta Garaje";
+        textoComb.text = "Combustible";
+        textoHUD.text = " 1 Pistola| 2 Escopeta| F Linterna | H Botiquin";
     }
 
-    private void recibirDaño()
+    private void recibirDaÃ±o()
     {
         hp = hp - 25;
         mostrarTextos();
@@ -80,13 +101,11 @@ public class ControlJugador : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Bot"))
         {
-            recibirDaño();
+            recibirDaÃ±o();
 
         }
 
     }
-
-   
 
     private void OnTriggerEnter(Collider other)
     {
@@ -115,14 +134,53 @@ public class ControlJugador : MonoBehaviour
         }
 
 
-        if (other.gameObject.CompareTag("Objetos") == true)
+        if (other.gameObject.CompareTag("Combustible") == true)
         {
             
             other.gameObject.SetActive(false);
             obj -= 1;
+            
             mostrarTextos();
+            textoComb.text = "Combustible (X)";
+            textoComb.color = Color.green;
         }
 
+        if (other.gameObject.CompareTag("Mapa") == true)
+        {
+
+            other.gameObject.SetActive(false);
+            obj -= 1;
+
+            mostrarTextos();
+            textoMapa.text = "Mapa (X)";
+            textoMapa.color = Color.green;
+        }
+        if (other.gameObject.CompareTag("Llave") == true)
+        {
+
+            other.gameObject.SetActive(false);
+            obj -= 1;
+
+            mostrarTextos();
+            textoLlave.text = "Llave Del Auto (X)";
+            textoLlave.color = Color.green;
+        }
+        if (other.gameObject.CompareTag("Tarjeta") == true)
+        {
+
+            other.gameObject.SetActive(false);
+            obj -= 1;
+
+            mostrarTextos();
+            textoTarjeta.text = "Tarjeta Garaje (X)";
+            textoTarjeta.color = Color.green;
+        }
+
+        if((other.gameObject.CompareTag("Salida") == true && obj == 0)){
+
+            Ganaste();
+
+        }
 
     }
 
@@ -153,7 +211,7 @@ public class ControlJugador : MonoBehaviour
                         ControlBot scriptObjetoTocado = (ControlBot)objetoTocado.GetComponent(typeof(ControlBot));
                         if (scriptObjetoTocado != null)
                         {
-                            scriptObjetoTocado.recibirDaño();
+                            scriptObjetoTocado.recibirDaÃ±o();
                         }
                     }
                 }
@@ -185,7 +243,7 @@ public class ControlJugador : MonoBehaviour
                         ControlBot scriptObjetoTocado = (ControlBot)objetoTocado.GetComponent(typeof(ControlBot));
                         if (scriptObjetoTocado != null)
                         {
-                            scriptObjetoTocado.recibirDañoEsc();
+                            scriptObjetoTocado.recibirDaÃ±oEsc();
                         }
                     }
                 }
@@ -225,6 +283,22 @@ public class ControlJugador : MonoBehaviour
         return set1;
     }
 
+    private void Reinicio()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void Ganaste()
+    {
+        Time.timeScale = 0;
+        
+    }
+
+    private void Perdiste()
+    {
+        Time.timeScale = 0;
+    }
+
 
     void Update()
     {
@@ -235,6 +309,19 @@ public class ControlJugador : MonoBehaviour
         movimientoAdelanteAtras *= Time.deltaTime; movimientoCostados *= Time.deltaTime;
         transform.Translate(movimientoCostados, 0, movimientoAdelanteAtras);
 
+
+        if (hp <= 0)
+        {
+            Perdiste();
+
+            if (Input.GetKey(KeyCode.R))
+            {
+                Reinicio();
+
+            }
+        }
+
+       
         if (Input.GetKey(KeyCode.LeftShift))
         {
             correr = !correr;
