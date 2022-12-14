4861c066bc9b6a18257b8c1ed59eb8fef583b116
FedericoCurutchet <114716003+FedericoCurutchet@users.noreply.github.com>
2022-12-14 15:12:45 -0300

    Funcion Mascara de Gas
    la mascara de gas lo que hace ahora es protegerte del los ataques de los enemigos, si se la tiene equipada quitaran menos vida.

--- Assets/Scripts/ControlJugador.cs
+++ Assets/Scripts/ControlJugador.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class ControlJugador : MonoBehaviour
 {
+    #region Variables
     public float rapidezDesplazamiento = 5.0f;
     public Camera camaraPrimeraPersona;
     public GameObject proyectil;
@@ -27,6 +28,8 @@ public class ControlJugador : MonoBehaviour
     public bool luzactivada = false;
     public bool correr;
     public bool set1 = true;
+    public bool masc = false;
+    
 
     public TMPro.TMP_Text textoMunicion;
     public TMPro.TMP_Text textoVida;
@@ -39,9 +42,11 @@ public class ControlJugador : MonoBehaviour
     public TMPro.TMP_Text textoHUD;
     public TMPro.TMP_Text textoPerdiste;
     public TMPro.TMP_Text textoCreditos;
+    public TMPro.TMP_Text textoControles;
     public RawImage PistolaLogo;
     public RawImage EscopetaLogo;
-
+    public RawImage FondoMenu;
+    public RawImage MascaraGas;
 
     public int valmaxp = 10;
     public int valmaxe = 6;
@@ -49,14 +54,18 @@ public class ControlJugador : MonoBehaviour
     public int munrecesc = 6;
     public int municion = 0;
     public int municionesc = 0;
-    public int hp;
+    public float hp;
     public int botiquin = 0;
     public int obj = 4;
     public float posicionY;
+    #endregion
     void Start()
     {
+        Debug.Log("Hello World!");
         PistolaLogo.gameObject.SetActive(true);
         EscopetaLogo.gameObject.SetActive(false);
+        FondoMenu.gameObject.SetActive(false);
+        MascaraGas.gameObject.SetActive(false);
         Linterna2.SetActive(false);
         Escopeta.SetActive(false);
         GestorDeAudio.instancia.ReproducirSonido("musica");
@@ -71,40 +80,90 @@ public class ControlJugador : MonoBehaviour
         textoHUD.text = "";
         textoPerdiste.text = "";
         textoCreditos.text = "";
+        textoControles.text = "";
         Cursor.lockState = CursorLockMode.Locked;
         mostrarTextos();
     }
-
-    public void mostrarTextos()
+    void Update()
     {
-        if (set1 == true)
-        {
-            textoMunicion.text = munrec.ToString() + "|" + municion.ToString();
-            PistolaLogo.gameObject.SetActive(true);
-            EscopetaLogo.gameObject.SetActive(false);
 
-        } else if (set1 == false)
+
+        float movimientoAdelanteAtras = Input.GetAxis("Vertical") * rapidezDesplazamiento;
+        float movimientoCostados = Input.GetAxis("Horizontal") * rapidezDesplazamiento;
+        movimientoAdelanteAtras *= Time.deltaTime; movimientoCostados *= Time.deltaTime;
+        transform.Translate(movimientoCostados, 0, movimientoAdelanteAtras);
+
+
+        if (hp <= 0)
         {
-            textoMunicion.text = munrecesc.ToString() + "|" + municionesc.ToString();
-            PistolaLogo.gameObject.SetActive(false);
-            EscopetaLogo.gameObject.SetActive(true);
+            Perdiste();
+
+            if (Input.GetKey(KeyCode.R))
+            {
+                Reinicio();
+
+            }
         }
 
-        textoVida.text = "VIDA: " + hp.ToString();
-        textoBotiquin.text = "Botiquines: " + botiquin.ToString();
-        textoObjetos.text = "Objetos Restantes: " + obj.ToString();
-        textoMapa.text = "Mapa";
-        textoLlave.text = "Llave del Auto";
-        textoTarjeta.text = "Tarjeta Garaje";
-        textoComb.text = "Combustible";
-        textoHUD.text = " 1 Pistola| 2 Escopeta| F Linterna | H Botiquin";
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+
+        }
+
+        if (Input.GetKey(KeyCode.H))
+        {
+            curarse();
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            luzactivada = !luzactivada;
+            if (luzactivada == true)
+            {
+                LuzLinterna.enabled = true;
+            }
+
+            if (luzactivada == false)
+            {
+                LuzLinterna.enabled = false;
+            }
+
+        }
+
+        if (set1 == true && Input.GetKey(KeyCode.R))
+        {
+            Recagar();
+        }
+        else if (set1 == false && Input.GetKey(KeyCode.R))
+        {
+            RecargarEsc();
+        }
+
+
+        ActivarMascara();
+        invocarTextos();
+        Despacio();
+        Correr();
+        Armas();
+        Disparar();
+        //Apuntar();
+
+
     }
+
+    #region Mecanicas
     private void Recagar()
     {
         int numaux;
         numaux = valmaxp - munrec;
 
-        if (municion > 0) {
+        if (municion > 0)
+        {
             if (munrec == 0)
             {
 
@@ -131,7 +190,6 @@ public class ControlJugador : MonoBehaviour
         mostrarTextos();
 
     }
-
     private void RecargarEsc()
     {
         int numaux;
@@ -162,15 +220,10 @@ public class ControlJugador : MonoBehaviour
 
         mostrarTextos();
     }
-    private void recibirDaño()
-    {
-        hp = hp - 25;
-        mostrarTextos();
-    }
-
     private void curarse()
     {
-        if (hp < 100 && botiquin >= 1) {
+        if (hp < 100 && botiquin >= 1)
+        {
 
             hp = 100;
             botiquin -= 1;
@@ -179,7 +232,6 @@ public class ControlJugador : MonoBehaviour
         }
 
     }
-
     private void Correr()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
@@ -194,7 +246,6 @@ public class ControlJugador : MonoBehaviour
             rapidezDesplazamiento = 5.0f;
         }
     }
-
     private void Despacio()
     {
         if (Input.GetKeyDown(KeyCode.C))
@@ -209,117 +260,14 @@ public class ControlJugador : MonoBehaviour
             rapidezDesplazamiento = 5.0f;
         }
     }
- 
-    private void OnCollisionEnter(Collision collision)
+    public void Disparar()
     {
-        if (collision.gameObject.CompareTag("Bot"))
-        {
-            recibirDaño();
-
-        }
-
-    }
-   /* public void Apuntar()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            camaraPrimeraPersona.fieldOfView = 15;
-            Escopeta.transform.position = new Vector3(0f, -0.4619999f, 1.293003f);
-           
-
-        }
-        if (Input.GetMouseButtonUp(1))
-        {
-            camaraPrimeraPersona.fieldOfView = 60;
-            Escopeta.transform.position = new Vector3(0.4109993f, -0.4619999f, 1.293003f);
-        }
-    } */
-    private void OnTriggerEnter(Collider other)
-    {
-
-        if (other.gameObject.CompareTag("Municion") == true)
-        {
-            other.gameObject.SetActive(false);
-            municion += 10;
-            mostrarTextos();
-
-        }
-        if (other.gameObject.CompareTag("Municionesc") == true)
-        {
-            other.gameObject.SetActive(false);
-            municionesc += 6;
-            mostrarTextos();
-
-        }
-
-        if (other.gameObject.CompareTag("Botiquin") == true)
-        {
-            GestorDeAudio.instancia.ReproducirSonido("botiquin");
-            other.gameObject.SetActive(false);
-            botiquin += 1;
-            mostrarTextos();
-        }
-
-
-        if (other.gameObject.CompareTag("Combustible") == true)
-        {
-            
-            other.gameObject.SetActive(false);
-            obj -= 1;
-            
-            mostrarTextos();
-            textoComb.text = "Combustible (X)";
-            textoComb.color = Color.green;
-        }
-
-        if (other.gameObject.CompareTag("Mapa") == true)
-        {
-
-            other.gameObject.SetActive(false);
-            obj -= 1;
-
-            mostrarTextos();
-            textoMapa.text = "Mapa (X)";
-            textoMapa.color = Color.green;
-        }
-        if (other.gameObject.CompareTag("Llave") == true)
-        {
-
-            other.gameObject.SetActive(false);
-            obj -= 1;
-
-            mostrarTextos();
-            textoLlave.text = "Llave Del Auto (X)";
-            textoLlave.color = Color.green;
-        }
-        if (other.gameObject.CompareTag("Tarjeta") == true)
-        {
-
-            other.gameObject.SetActive(false);
-            obj -= 1;
-
-            mostrarTextos();
-            textoTarjeta.text = "Tarjeta Garaje (X)";
-            textoTarjeta.color = Color.green;
-        }
-
-        if((other.gameObject.CompareTag("Salida") == true && obj == 0))
-        {
-
-            Ganaste();
-            
-
-        }
-
-    }
-
-    public void Disparar() {
         if (set1 == true)
         {
 
             if (Input.GetMouseButtonDown(0) && munrec > 0)
             {
-               
+
                 Ray ray = camaraPrimeraPersona.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
                 GameObject pro;
                 pro = Instantiate(proyectil, ray.origin, transform.rotation);
@@ -381,12 +329,9 @@ public class ControlJugador : MonoBehaviour
             }
         }
     }
-
-
-
     public bool Armas()
     {
-        
+
         if (Input.GetKey(KeyCode.Alpha1))
         {
             Pistola.SetActive(true);
@@ -412,12 +357,48 @@ public class ControlJugador : MonoBehaviour
 
         return set1;
     }
-
-    private void Reinicio()
+    private void ActivarMascara()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+        if (Input.GetKey(KeyCode.X))
+        {
+            MascaraGas.gameObject.SetActive(true);
+            masc = true;
 
+        }
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+
+            MascaraGas.gameObject.SetActive(false);
+            masc = false;
+        }
+
+
+    }
+    #endregion
+
+    #region HUD
+    public void mostrarTextos()
+    {
+        if (set1 == true)
+        {
+            textoMunicion.text = munrec.ToString() + "|" + municion.ToString();
+            PistolaLogo.gameObject.SetActive(true);
+            EscopetaLogo.gameObject.SetActive(false);
+
+        }
+        else if (set1 == false)
+        {
+            textoMunicion.text = munrecesc.ToString() + "|" + municionesc.ToString();
+            PistolaLogo.gameObject.SetActive(false);
+            EscopetaLogo.gameObject.SetActive(true);
+        }
+
+        textoVida.text = "VIDA: " + hp.ToString();
+        textoBotiquin.text = "Botiquines: " + botiquin.ToString();
+        textoHUD.text = "J| Controlesar \n" +
+            "K| Objetivo";
+    }
     private void Ganaste()
     {
 
@@ -447,10 +428,10 @@ public class ControlJugador : MonoBehaviour
             "Guns Pack: Low Poly Guns Collection de Fun Assets \n" +
             "GAZ Street Props de Helsssoo \n" +
             "Un videojuego de Federico Curutchet \n";
-      
+
         Time.timeScale = 0;
-       
-        
+
+
     }
 
     private void Perdiste()
@@ -463,72 +444,156 @@ public class ControlJugador : MonoBehaviour
         mostrarTextos();
     }
 
-
-    void Update()
+    private void invocarTextos()
     {
-        
-
-        float movimientoAdelanteAtras = Input.GetAxis("Vertical") * rapidezDesplazamiento;
-        float movimientoCostados = Input.GetAxis("Horizontal") * rapidezDesplazamiento;
-        movimientoAdelanteAtras *= Time.deltaTime; movimientoCostados *= Time.deltaTime;
-        transform.Translate(movimientoCostados, 0, movimientoAdelanteAtras);
-
-
-        if (hp <= 0)
+        if (Input.GetKeyDown(KeyCode.J))
         {
-            Perdiste();
+            textoControles.text = "Controles: \n" +
+           "1| Pistola \n" +
+           "2| Escopeta \n" +
+           "F| Linterna\n" +
+           "H| Curarse \n" +
+           "R| Recargar \n" +
+           "Click Izquierdo| Disparar \n" +
+           "Click Derecho| Apuntar \n" +
+           "C| Agacharse \n" +
+           "Shift| Correr\n";
 
-            if (Input.GetKey(KeyCode.R))
-            {
-                Reinicio();
-
-            }
+            FondoMenu.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.J))
+        {
+            textoControles.text = "";
+            FondoMenu.gameObject.SetActive(false);
         }
 
-
-        if (Input.GetKeyDown("escape"))
+        if (Input.GetKeyDown(KeyCode.K))
         {
-            Cursor.lockState = CursorLockMode.None;
-
+            textoObjetos.text = "Encuentra los siguientes objetos \n " +
+            "para escapar con el auto: " + obj.ToString();
+            textoMapa.text = "Mapa";
+            textoLlave.text = "Llave del Auto";
+            textoTarjeta.text = "Tarjeta Garaje";
+            textoComb.text = "Combustible";
+        }
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            textoObjetos.text = "";
+            textoMapa.text = "";
+            textoLlave.text = "";
+            textoTarjeta.text = "";
+            textoComb.text = "";
         }
 
-       if(Input.GetKey(KeyCode.H))
-        {
-            curarse();
-
-        }
-
-        
-
-        if (Input.GetKey(KeyCode.F))
-        {
-            luzactivada = !luzactivada;
-            if (luzactivada == true)
-            {
-                LuzLinterna.enabled = true;
-            }
-
-            if (luzactivada == false)
-            {
-                LuzLinterna.enabled = false;
-            }
-
-        }
-
-        if (set1 == true && Input.GetKey(KeyCode.R))
-        {
-            Recagar();
-        }else if (set1 == false && Input.GetKey(KeyCode.R))
-        {
-            RecargarEsc();
-        }
-
-        Despacio();
-        Correr();
-        Armas();
-        Disparar();
-        //Apuntar();
-       
-        
     }
+    #endregion
+    private void Reinicio()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    private void recibirDaño()
+    {
+        if(masc == false)
+        {
+            hp = hp - 25;
+            mostrarTextos();
+        }
+
+        if (masc == true)
+        {
+            hp = hp - 10;
+            mostrarTextos();
+        }
+      
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bot"))
+        {
+            recibirDaño();
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Municion") == true)
+        {
+            other.gameObject.SetActive(false);
+            municion += 10;
+            mostrarTextos();
+
+        }
+        if (other.gameObject.CompareTag("Municionesc") == true)
+        {
+            other.gameObject.SetActive(false);
+            municionesc += 6;
+            mostrarTextos();
+
+        }
+
+        if (other.gameObject.CompareTag("Botiquin") == true)
+        {
+            GestorDeAudio.instancia.ReproducirSonido("botiquin");
+            other.gameObject.SetActive(false);
+            botiquin += 1;
+            mostrarTextos();
+        }
+
+
+        if (other.gameObject.CompareTag("Combustible") == true)
+        {
+
+            other.gameObject.SetActive(false);
+            obj -= 1;
+
+            textoComb.text = "Combustible (X)";
+            textoComb.color = Color.green;
+        }
+
+        if (other.gameObject.CompareTag("Mapa") == true)
+        {
+
+            other.gameObject.SetActive(false);
+            obj -= 1;
+
+
+            textoMapa.text = "Mapa (X)";
+            textoMapa.color = Color.green;
+        }
+        if (other.gameObject.CompareTag("Llave") == true)
+        {
+
+            other.gameObject.SetActive(false);
+            obj -= 1;
+
+            textoLlave.text = "Llave Del Auto (X)";
+            textoLlave.color = Color.green;
+        }
+        if (other.gameObject.CompareTag("Tarjeta") == true)
+        {
+
+            other.gameObject.SetActive(false);
+            obj -= 1;
+
+
+            textoTarjeta.text = "Tarjeta Garaje (X)";
+            textoTarjeta.color = Color.green;
+        }
+
+        if ((other.gameObject.CompareTag("Salida") == true && obj == 0))
+        {
+
+            Ganaste();
+
+
+        }
+
+       
+
+    }
+
 }
